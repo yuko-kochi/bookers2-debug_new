e01ad7898526f69226353f5c9b66b8f3add3d2c2
yuko-kochi <happy.k165131@gmail.com>
2021-07-04 05:27:59 +0000

    favorites_comments

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,24 @@
+class BookCommentsController < ApplicationController
+  
+	def create
+		@book = Book.find(params[:book_id])
+		@book_comment = BookComment.new(book_comment_params)
+		@book_comment.book_id = @book.id
+		@book_comment.user_id = current_user.id
+		if @book_comment.save
+  		redirect_to book_path(@book.id)
+		else
+		  render 'books/show'
+		end
+	end
+
+  def destroy
+    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    redirect_to book_path(params[:book_id])
+  end
+
+  private
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
   
   
 end
