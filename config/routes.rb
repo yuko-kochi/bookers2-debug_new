171eb0e381cbbd76b29de5ae2da873e8108acf33
yuko-kochi <happy.k165131@gmail.com>
2021-07-04 07:57:27 +0000

    follow

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,18 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
   
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers' 
+  end
+  
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+  
+
   
   
 end
